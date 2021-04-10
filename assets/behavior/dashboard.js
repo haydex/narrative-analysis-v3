7d8974ad25c75ebb6b23b63ff2051fe6cecde1e9
Haydex <hayddex@gmail.com>
2021-04-09 21:34:53 -0500

    Dashboard Window

---
+++ assets/behavior/dashboard.js
@@ -0,0 +1,44 @@
+/*
+
+
+
+	" Reimplement the wheel to either learn, or make it better. "
+
+    https://www.hayder.io/
+    https://www.youtube.com/watch?v=QOlTGA3RE8I
+    
+    Product Name: Btracker
+	Description: Tracking Blog's data.
+	Beneficiary: COSMOS
+	
+	Copyright © 1988 - 2021 HAYDEX, All Rights Reserved.
+	
+	
+	
+*/
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Narrative Tree Dashboard
+
+    class NarrativeTreeDashboard {
+
+        constructor() {
+
+            this.initialize();
+
+        }
+
+        initialize() {
+
+        }
+
+    }
+
+    // Initialization
+
+    let narrativeTreeDashboard = new NarrativeTreeDashboard();
+
+});
